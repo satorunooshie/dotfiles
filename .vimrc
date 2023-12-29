satorunooshie/dotfiles

commit 1794985758175135bf064787b1aa7dda5fa2320e

improve mru to filter git root

.vimrc
@@ -126,7 +126,7 @@ def! g:UpdatePackPlugins(): void #{{{
   var nr: number = bufadd('[update plugins]')
   bufload(nr)
   execute ':' .. nr .. 'sb'
-  setlocal buftype=nofile
+  ToScratch
 
   def PluginUpdateHandler(timer: any): void #{{{
     const plugin_dir = expand($PACKPATH .. '/' .. 'opt')
@@ -501,6 +501,13 @@ set hlsearch
 # ---------------------------------------------------------------------------
 # Files: #{{{
 #
+var git_root = ''
+command! -bar SetGitRoot git_root = system('git rev-parse --show-toplevel')
+  | if git_root =~# '^fatal'
+  | git_root = ''
+  | else
+  | git_root = git_root->trim()->expand()
+  | endif
 var files_cmd = 'find '
 var files_opts = '-type f'
 command! -bar ToScratch setlocal buftype=nofile bufhidden=hide noswapfile
@@ -512,10 +519,10 @@ command! -bar -nargs=? ModsNew <mods> new
   | edit [<args>]
   | endif
 command! MRU <mods> ModsNew MRU
+  | SetGitRoot
   | ToScratchForFiles
-  | setline(1, filter(v:oldfiles, (_, v: string) => filereadable(expand(v))))
-command! MRUQuickFix setqflist(map(filter(v:oldfiles, (_, v: string) => filereadable(expand(v))), '{"filename": expand(v:val)}'))
-  | copen
+  | setline(1, v:oldfiles->filter((_, v: string) => filereadable(expand(v)))->mapnew((_, v: string) => v->trim()->expand()->substitute(expand('$HOME'), '~', ''))->filter((_, v: string) => (empty(git_root) || expand(v) =~# git_root) && v !~# '\.git/'))
+#command! MRUQuickfix setqflist(map(filter(v:oldfiles, (_, v: string) => filereadable(expand(v))), '{"filename": expand(v:val), "efm": "%f"}')) | copen
 command! -nargs=1 -complete=command L <mods> ModsNew <args>
   | ToScratchForFiles
   | setline(1, split(execute(<q-args>), '\n'))

commit f1d5b46d64e6c87df8298ce60af657e070bd3269

add git root cache to fix startup performance issue
caused by 1794985758175135bf064787b1aa7dda5fa2320e

```
-----------------------------
Before: 4b6df1a7dbecad6ec72fb436d55c87b91bde0f6b
-----------------------------
satorukitaguchi@Satoru-MacBook-Pro ~ % vim-startuptime -script -count 1000
Extra options: []
Measured: 1000 times

Total Average: 24.807027 msec
Total Max:     55.881000 msec
Total Min:     18.092000 msec

  AVERAGE       MAX      MIN
-----------------------------
13.078396 33.902000 9.331000: $HOME/.vimrc

-----------------------------
After: 1794985758175135bf064787b1aa7dda5fa2320e
-----------------------------

Total Average: 28.146337 msec
Total Max:     110.643000 msec
Total Min:     17.852000 msec

  AVERAGE       MAX      MIN
-----------------------------
14.988843 73.651000 9.332000: $HOME/.vimrc

-----------------------------
This commit
-----------------------------
Total Average: 17.607571 msec
Total Max:     21.644000 msec
Total Min:     16.635000 msec

 AVERAGE       MAX      MIN
----------------------------
9.381277 12.629000 8.748000: $HOME/.vimrc
```

.vimrc
@@ -374,6 +374,7 @@ set nolist
 set listchars=tab:>-,extends:<,precedes:>,trail:-,eol:$,nbsp:%
 
 # Tabline settings. #{{{
+g:current_dir = fnamemodify(getcwd(), ':p:~:h') .. ' '
 def! g:MakeTabLine(): string #{{{
   def TabpageLabel(n: number): string #{{{
     const title = gettabwinvar(n, 0, 'title')
@@ -393,8 +394,8 @@ def! g:MakeTabLine(): string #{{{
   const titles: list<string> = map(range(1, tabpagenr('$')), (_, bufnr: number) => TabpageLabel(bufnr))
   const sep = ' | '
   const tabpages = join(titles, sep) .. sep .. '%#TabLineFill#%T'
-  const current_dir = fnamemodify(getcwd(), '~:') .. ' '
-  return tabpages .. '%=' .. current_dir
+  g:current_dir = fnamemodify(getcwd(), ':p:~:h') .. ' '
+  return tabpages .. '%=' .. g:current_dir
 enddef #}}}
 
 set tabline=%!MakeTabLine()
@@ -502,11 +503,15 @@ set hlsearch
 # Files: #{{{
 #
 var git_root = ''
-command! -bar SetGitRoot git_root = system('git rev-parse --show-toplevel')
+var git_root_cache = ''
+command! -bar SetGitRoot if g:current_dir !=# git_root_cache
+  | git_root_cache = g:current_dir
+  | git_root = system('git rev-parse --show-toplevel')
   | if git_root =~# '^fatal'
   | git_root = ''
   | else
   | git_root = git_root->trim()->expand()
+  | endif
   | endif
 var files_cmd = 'find '
 var files_opts = '-type f'